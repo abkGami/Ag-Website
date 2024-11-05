dc64d4dd12765042c0dfb7bd3145ced361853807
abkgami <yahabubakar1@gmail.com>
2024-11-05 22:15:33 +0100

    abcd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import logo from "../assets/Icon.png";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [isMenuOpen, setisMenuOpen] = useState(false);
+  const [isSticky, setisSticky] = useState(false);
+
+  const toggleMenu = () => {
+    setisMenuOpen(!isMenuOpen);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setisSticky(true);
+      } else {
+        setisSticky(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.addEventListener("scroll", handleScroll);
+    };
+  });
+
+  const navItems = [
+    { link: "Home", path: "home" },
+    { link: "Service", path: "service" },
+    { link: "About", path: "about" },
+    { link: "Product", path: "product" },
+    { link: "Testimonial", path: "testimonial" },
+    { link: "FAQ", path: "faq" },
+  ];
+
+  return (
+    <header className="w-full bg-white md:bg-transparent fixed top-0 left-0 right-0">
+      <nav>
+        <a
+          href=""
+          className="text-2xl font-semibold flex items-center space-x-3"
+        >
+          <img src={logo} alt="" className="w-10 inline-block items-center" />
+          <span className="text-[#263238]">NEXCENT</span>
+
+          {/* nav items for large devices */}
+          <ul className="md:flex space-x-12 hidden">
+            {navItems.map(({ link, path }) => (
+              <Link key={path} to={path}>
+                {link}
+              </Link>
+            ))}
+          </ul>
+        </a>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
