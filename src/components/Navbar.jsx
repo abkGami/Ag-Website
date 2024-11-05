2ad625285e83fc75d74a1ddab6ead7b3e33ad9a2
abkgami <yahabubakar1@gmail.com>
2024-11-05 23:18:22 +0100

    LG

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import logo from "../assets/Icon.png";
 import { Link } from "react-scroll";
 
+import { FaXmark, FaBars } from "react-icons/fa6";
+
 const Navbar = () => {
   const [isMenuOpen, setisMenuOpen] = useState(false);
   const [isSticky, setisSticky] = useState(false);
@@ -37,12 +39,14 @@ const Navbar = () => {
   return (
     <header className="w-full bg-white md:bg-transparent fixed top-0 left-0 right-0">
       <nav>
-        <a
-          href=""
-          className="text-2xl font-semibold flex items-center space-x-3"
-        >
-          <img src={logo} alt="" className="w-10 inline-block items-center" />
-          <span className="text-[#263238]">NEXCENT</span>
+        <div>
+          <a
+            href=""
+            className="text-2xl font-semibold flex items-center space-x-3"
+          >
+            <img src={logo} alt="" className="w-10 inline-block items-center" />
+            <span className="text-[#263238]">NEXCENT</span>
+          </a>
 
           {/* nav items for large devices */}
           <ul className="md:flex space-x-12 hidden">
@@ -59,7 +63,22 @@ const Navbar = () => {
               </Link>
             ))}
           </ul>
-        </a>
+          <div className="space-x-12 hidden lg:flex items-center">
+            <a
+              href="/"
+              className="hidden lg:flex items-center text-BrandPrimary
+            hover:text-grey900"
+            >
+              Login
+            </a>
+            <button
+              className="bg-BrandPrimary text-white py-2 px-4 transition-all duration-300
+            rounded hover:bg-NeutralDGrey"
+            >
+              SignUp
+            </button>
+          </div>
+        </div>
       </nav>
     </header>
   );
