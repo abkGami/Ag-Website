c664bb2a3c2967397134fd60bbe85d7c8212d152
abkgami <yahabubakar1@gmail.com>
2024-11-07 21:37:42 +0100

    yes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
               <p className="text-NeutralGrey text-base mb-8">
                 {" "}
                 Where to grow your business as a photographer: site or social
-                media?
+                mediapage media?
               </p>
               <button className="px-7 py-2 bg-BrandPrimary text-white rounded hover:bg-NeutralGrey transition-all duration-300 hover:-translate-y-4">
                 Register
