779ea260d664317af3accf33cec234b7856f467d
abkgami <yahabubakar1@gmail.com>
2024-11-08 23:06:44 +0100

    ree

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
                 mediapage media?
               </p>
               <button className="px-7 py-2 bg-BrandPrimary text-white rounded hover:bg-NeutralGrey transition-all duration-300 hover:-translate-y-4">
-                Registers
+                Register
               </button>
             </div>
           </div>
